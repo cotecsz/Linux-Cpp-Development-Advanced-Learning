bc70e6f847ab46c7e044239fbdce7a7432e40f2f
Shuangzhen <dreamre21@gmail.com>
2019-08-20 21:05:25 +0800

    Add python head info

--- MachineLearning/LogisticRegression/LogisticRegression.py
+++ MachineLearning/LogisticRegression/LogisticRegression.py
@@ -1,4 +1,10 @@
-<<<<<<< HEAD
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+
+# @Time   : 2019/8/16 11:14
+# @Author  : Yang Shuangzhen
+# @File   : LogisticRegression.py
+
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
@@ -11,12 +17,5 @@ print('len(y):{length}, shape(y):{shape}'.format(length=len(y), shape=y.shape))
 
 plt.scatter(X[:, :1], y, alpha=0.6)
 plt.show()
-=======
-#!/usr/bin/python3
-# -*-coding:utf-8 -*-
 
-# @Time   : 2019/8/16 11:14
-# @Author  : Yang Shuangzhen
-# @File   : LogisticRegression.py
 
->>>>>>> 3959641f65def352a92c89fe6a6f4635fc70366e
